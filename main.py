11166d214fc9cdb3410ba84d6c463b174555414b
DasHenning <h.h.reckentin@icloud.com>
2025-05-27 20:18:58 +0200

    edited Output looks

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 from stats import *
 
 def main():
-    wordCount = CountWords("books/frankenstein.txt")
-    print(f"{wordCount} words found in the document")
+    print("============ BOOKBOT ============\nAnalyzing book found at books/frankenstein.txt...")
 
-    print(CountCharacters("books/frankenstein.txt"))
+    file = "books/frankenstein.txt"
+    wordCount = CountWords(file)
+    charDict = CountCharacters(file)
+    print(f"----------- Word Count ----------\nFound {wordCount} total words")
+
+    charList = [] 
+    for entry in charDict:
+        charList.append({"char":entry, "count":charDict[entry]})
+
+    charList.sort(key=lambda dict: dict["count"], reverse=True)
+
+    print("----------- Word Count ----------")
+    for elem in charList:
+        if elem["char"].isalpha():
+            print(f"{elem["char"]}: {elem["count"]}")
+    print("============= END ===============")
 
 main()
